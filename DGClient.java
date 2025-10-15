a0df1b501ad14974d5b0d31845196e14050bcea3
Vigneshsmvitm <vignesh.23cs026@sode-edu.in>
2025-10-15 14:07:34 +0530

    DGClient.java

--- DGClient.java
+++ DGClient.java
@@ -9,13 +9,25 @@ package dgclient;
  *
  * @author Lab6
  */
-public class DGClient {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+import java.net.*;
+public class DGClient
+{
+    public static void main(String[] args) throws Exception 
+    {
+        String line = "connected with client";
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+        sendData = line.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9000);
+        clientSocket.send(sendPacket);
+        System.out.println("******client display terminal******");
+        while(true) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+            String messageReceived = new String(receivePacket.getData(), receivePacket.getOffset(), receivePacket.getLength());
+            System.out.println("Message received from server is: " + messageReceived);
+        }
     }
-    
 }
